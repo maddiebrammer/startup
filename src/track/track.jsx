485c80e0c856e561400a57f9e25bbdb29239721f
mads66 <carter36611@gmail.com>
2025-10-07 17:52:58 -0600

    Init individual jsx files

---
+++ src/track/track.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Track() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>tracking displayed here</div>
+    </main>
+  );
+}
